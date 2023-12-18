f61b16bf480433d6d7ae01898fcc8bc3067daca2
Fahima <232140@astanait.edu.kz>
2023-12-18 11:52:12 +0600

    created classes point and shape

---
+++ src/models/Point.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Point {
+    private double x;
+    private double y;
+
+// constructor having both parameters
+// distance(Point dest) - returns the distance from current point to the dest
+
+// toString()
